5c7162199795d601a5ddfead6c6f9430eb37ffb4
quikerquiker99 <quikerquiker@outlook.in>
2024-10-06 21:07:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python3 /workspaces/VV/serverr.py",
+    "postStartCommand": "python3 /workspaces/pp/serverr.py",
     "customizations": {
         "vscode": {
             "settings": {
